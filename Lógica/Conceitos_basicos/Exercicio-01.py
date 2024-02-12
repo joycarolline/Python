e1a69057eec203fbf3bf2ab2a98649e07b0ca6da
Joyce Andrade <jcaroline.andrade@gmail.com>
2024-02-11 23:58:27 -0300

    atualização exercicios conceitos basicos

---
+++ Lógica/Conceitos_basicos/Exercicio-01.py
@@ -0,0 +1,16 @@
+'''Faça um Programa que peça dois números, realize as principais operações soma, 
+subtração, multiplicação, divisão.'''
+
+
+print("Descubra o resultado das principais operações matemáticas entre dois numeros:  \n")
+
+num1 = int(input("Digite o primeiro número: " ));
+num2 = int(input("Digite o primeiro número: " ));
+
+soma = num1+num2;
+sub = num1-num2;
+mult = num1*num2;
+div = num1/num2;
+
+print(f"O resultado é: \n soma = {soma}; \n subtração = {sub}; \n multiplicação = {mult}; \n divisão = {div}")
+
