2748d37ee154d527352f390d072cc63e5051d4ad
Joyce Andrade <jcaroline.andrade@gmail.com>
2024-02-15 02:09:40 -0300

    Adicionando novo exercicio

---
+++ Lógica/Conceitos_basicos/Exercicio-02.py
@@ -0,0 +1,10 @@
+'''Peça ao usuário para informar o ano de nascimento.
+Em seguida, calcule e imprima a idade atual.'''
+
+print("Descubra a sua idade no ano de 2024:  \n")
+
+nascimento = int(input("Digite o seu ano de nascimento: "));
+
+idade = 2024 - nascimento 
+
+print(f"Em 2024 voce fará: {idade} anos")
